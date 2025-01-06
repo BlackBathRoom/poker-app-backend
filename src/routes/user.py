319c20e19cbe084af8732a94ee2f42c3197b5c9c
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-01-06 23:17:04 +0900

    ユーザーリソースを改築

--- src/routes/user.py
+++ src/routes/user.py
@@ -1,18 +1,22 @@
-from typing import Any, get_type_hints
+from math import e
+from typing import Any, TypeVar, Union, cast
 
-from flask import request, Blueprint, Response
-from flask.json import loads
+from flask import Blueprint, Response
 from flask_restful import Api
+from pydantic_core import ValidationError
 
 from exceptios import UserNotFoundError
 from routes.base_resource import BaseResource
 from model.user import UserDBManager, UserInfo
+from schema import OptionalUserInfo
 
 
 app = Blueprint("users", __name__)
 api = Api(app)
 
 # Userリソース
+
+T = TypeVar("T", bound=Union[UserInfo, OptionalUserInfo])
 
 class UserResource(BaseResource):
     def __init__(self) -> None:
@@ -28,36 +32,30 @@ class UserResource(BaseResource):
         else:
             user = self.db.user_list()
 
-        return self.success_response(200, data=user)
+        return self.success_response(
+            200,
+            data=user.model_dump() if isinstance(user, UserInfo) else [u.model_dump() for u in user]
+        )
 
     def post(self) -> Response:
-        data = loads(request.data.decode("utf-8"))
+        data = self._request_formatter(
+            data=self.request_loader(),
+            into=UserInfo
+        )
         try:
-            _id = self.db.add_user(self._request_formatter(data))
-        except ValueError as e:
-            self.error_response(400, f"Missing Keys: {e}")
+            _id = self.db.add_user(data)
         except UserNotFoundError:
             self.error_response(400, "User not found")
         
         return self.success_response(201, data={"id": _id})
     
-    def _request_formatter(self, data: Any) -> UserInfo:
-        try:
-            user = {
-                "name": data["name"],
-                "chip": data["chip"],
-                "role": data["role"],
-                "isplaying": data["isplaying"],
-            }
-        except KeyError:
-            keys = list(get_type_hints(UserInfo).keys())
-            raise ValueError(f"{set(keys) - set(data.keys())}")
-        return user
-    
     def put(self, user_id: str) -> Response:
-        data = loads(request.data.decode("utf-8"))
+        data = self._request_formatter(
+            data=self.request_loader(),
+            into=OptionalUserInfo
+        )
         try:
-            self.db.update_user(user_id, **self._request_formatter(data))
+            self.db.update_user(user_id, data)
         except UserNotFoundError:
             self.error_response(400, "User not found")
         return self.success_response(204)
@@ -68,43 +66,66 @@ class UserResource(BaseResource):
         except UserNotFoundError:
             self.error_response(404, "User not found")
         return self.success_response(204)
+    
+    def _request_formatter(self, data: Any, into: type[T]) -> T:
+        if not self.request_data_checker(data):
+            self.error_response(400, "Invalid request data")
+
+        try:
+            if into == UserInfo:
+                user = UserInfo(**data)
+            else:
+                user = OptionalUserInfo(**data)
+        except ValidationError as e:
+            self.error_response(400, f"Invalid request data: {e}")
+
+        return cast(T, user)
 
 
 # サブリソース
 
 class UserSubResource(BaseResource):
 
-    sub_resource = list(get_type_hints(UserInfo).keys())
+    sub_resource = list(UserInfo.__annotations__.keys())
 
     def __init__(self) -> None:
         super().__init__()
         self.db = UserDBManager()
 
-    def _resource_type_checker(self, resource_type: str) -> bool:
+    def _resource_checker(self, resource_type: str) -> bool:
         if resource_type not in self.sub_resource:
             return False
         return True
     
+    def _data_loader(self) -> OptionalUserInfo:
+        data = self.request_loader()
+        if not self.request_data_checker(data):
+            self.error_response(400, "Invalid request data")
+        try:
+            user = OptionalUserInfo(**data)
+        except ValidationError as e:
+            self.error_response(400, f"Invalid request data: {e}")
+        return user
+    
     def get(self, user_id: str, resource_type: str) -> Response:
-        if not self._resource_type_checker(resource_type):
+        if not self._resource_checker(resource_type):
             self.error_response(404, "Invalid resource type")
         try:
-            user = self.db.user_by_id(user_id)
+            user = self.db.user_detail_by_id(user_id, resource_type)
         except UserNotFoundError:
             self.error_response(404, "User not found")
-
         return self.success_response(
             code=200,
-            data={resource_type: user[resource_type]},
+            data=user.model_dump(exclude_unset=True)
         )
 
     def put(self, user_id: str, resource_type: str) -> Response:
-        if not self._resource_type_checker(resource_type):
+        if not self._resource_checker(resource_type):
             self.error_response(404, "Invalid resource type")
-        data = loads(request.data.decode("utf-8"))
 
+        user = self._data_loader()
         try:
-            self.db.update_user(user_id, **{resource_type: data[resource_type]})
+            self.db.update_user(user_id, user)
         except KeyError:
             self.error_response(400, "Missing Keys")
         return self.success_response(204)
