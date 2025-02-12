45d1611e6080f3ea5665da72fb7c4465c759a8eb
haruki26 <ktc24a31c0004@edu.kyoto-tech.ac.jp>
2025-02-13 03:54:07 +0900

    corsを設定
    開発用なので後々丁寧に設定

--- src/app.py
+++ src/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from flask_cors import CORS
 
 from routes import user, game_info
 
@@ -6,6 +7,20 @@ from routes import user, game_info
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False
+CORS(
+    app,
+    resources={
+        r"/*": {
+            "origins": [
+                "http://localhost:5000",
+                "http://127.0.0.1:5000",
+            ],
+            "methods": ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+            "allow_headers": ["Content-Type"],
+        }
+    },
+
+)
 
 app.register_blueprint(user.app, url_prefix="/users")
 app.register_blueprint(game_info.app, url_prefix="/gameinfo")
